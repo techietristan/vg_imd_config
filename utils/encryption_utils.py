ad921e3743002b3953b19af3e57453921e448173
Tristan Davis <techietristan@gmail.com>
2024-09-28 04:10:29 -0600

    fix: handle invalid token in decrypt function

--- utils/encryption_utils.py
+++ utils/encryption_utils.py
@@ -1,8 +1,10 @@
 import base64, os
 from getpass import getpass
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+encoding_format: str = 'utf-8'
 
 def calculate_key(salt: bytes, passphrase: str):
     key_derivation_function: callable = PBKDF2HMAC(
@@ -11,24 +13,24 @@ def calculate_key(salt: bytes, passphrase: str):
         salt =          salt,
         iterations =    64 #16777216,
     )
-    encoded_passphrase: bytes = passphrase.encode('utf-8')
+    encoded_passphrase: bytes = passphrase.encode(encoding_format)
     base64_key: bytes = base64.urlsafe_b64encode(key_derivation_function.derive(encoded_passphrase))
     encryption_key: cryptography.fernet.Fernet = Fernet(base64_key)
     
     return encryption_key
 
 def encrypt(passphrase: str, text_to_encrypt: str) -> tuple[bytes, bytes]:
-    encoded_passphrase: bytes = passphrase.encode('utf-8')
+    encoded_passphrase: bytes = passphrase.encode(encoding_format)
     salt: bytes = os.urandom(16)
     encryption_key = calculate_key(salt, passphrase)
-    encrypted_text: bytes = encryption_key.encrypt(text_to_encrypt.encode('utf-8'))
+    encrypted_text: bytes = encryption_key.encrypt(text_to_encrypt.encode(encoding_format))
 
     return salt, encrypted_text
 
-def decrypt(salt: bytes, encrypted_text: bytes):
-    decrypt_passphrase= getpass('Please enter the encryption passphrase: ')
-    decryption_key = calculate_key(salt, decrypt_passphrase)
-    decrypted_text = decryption_key.decrypt(encrypted_text)
-
-    return decrypted_text
-
+def decrypt(salt: bytes, encrypted_text: bytes, passphrase: str) -> bytes | bool:
+    try:
+        decryption_key = calculate_key(salt, decrypt_passphrase)
+        decrypted_text = decryption_key.decrypt(encrypted_text).decode(encoding_format)
+        return decrypted_text
+    except InvalidToken:
+        return False
